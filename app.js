7a0504818bcffab46ccc69ad11f6444d61a2b45a
Mike K <m.kermes89@gmail.com>
2018-06-20 14:48:35 -0700

    Putting lab together

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+Product.all = [];
+
+function Product(name, path){
+    this.name = name;
+    this.path = path;
+    this.vote = 0;
+    this.seen = 0;
+    Produc.all.push(this);
+}
