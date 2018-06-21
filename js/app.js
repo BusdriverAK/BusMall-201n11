aacdb3e478ec912435fdc7b2e2a2a1d7bdfbf8e5
Mike K <m.kermes89@gmail.com>
2018-06-20 21:42:04 -0700

    started css, fixed linter spacing issues that popped up

--- js/app.js
+++ js/app.js
@@ -14,132 +14,132 @@ var getInfo;
 var getClick;
 
 function Product(name){
-    this.name = name;
-    this.path;
-    this.votes = 0;
-    this.seen = 0;
-    if(name === 'usb'){
-        this.path = 'assets/' + name + '.gif';
-    }else if(name === 'sweep'){
-        this.path = 'assets/' + name + '.png';
-    }else{
-        this.path = 'assets/' + name + '.jpg';
-    };
-    Product.all.push(this);
-};
+  this.name = name;
+  this.path;
+  this.votes = 0;
+  this.seen = 0;
+  if(name === 'usb'){
+    this.path = 'assets/' + name + '.gif';
+  }else if(name === 'sweep'){
+    this.path = 'assets/' + name + '.png';
+  }else{
+    this.path = 'assets/' + name + '.jpg';
+  }
+  Product.all.push(this);
+}
 
 function buildProducts(){
-    for (var i = 0; i < Product.names.length; i++){
-        new Product(Product.names[i]);
-    }
-};
+  for (var i = 0; i < Product.names.length; i++){
+    new Product(Product.names[i]);
+  }
+}
 
 function showItems(){
-    var currentItems = [];
-    Product.voteCount = [];
-    for(var i = 0; i < Product.names.length; i++){
-        Product.voteCount.push(Product.all[i].votes);
-    }
-// run checks to see if new image was used for last seen
+  var currentItems = [];
+  Product.voteCount = [];
+  for(var i = 0; i < Product.names.length; i++){
+    Product.voteCount.push(Product.all[i].votes);
+  }
+  // run checks to see if new image was used for last seen
 
+  currentItems[0] = Randomize();
+  while(Product.lastSeen.indexOf(currentItems[0]) !== -1){
+    console.log('duplicate found on left');
     currentItems[0] = Randomize();
-    while(Product.lastSeen.indexOf(currentItems[0]) !== -1){
-        console.log('duplicate found on left');
-        currentItems[0] = Randomize();
-    }
+  }
 
+  currentItems[1] = Randomize();
+  while(currentItems[0] === currentItems[1] || Product.lastSeen.indexOf(currentItems[1]) !== -1) {
+    console.log('duplicate found on center');
     currentItems[1] = Randomize();
-    while(currentItems[0] === currentItems[1] || Product.lastSeen.indexOf(currentItems[1]) !== -1) {
-        console.log('duplicate found on center');
-        currentItems[1] = Randomize();
-    }
+  }
 
+  currentItems[2] = Randomize();
+  while(currentItems[0] === currentItems[2] || currentItems[1] === currentItems[2] || Product.lastSeen.indexOf(currentItems[2]) !== -1){
+    console.log('duplicate found on right');
     currentItems[2] = Randomize();
-    while(currentItems[0] === currentItems[2] || currentItems[1] === currentItems[2] || Product.lastSeen.indexOf(currentItems[2]) !== -1){
-        console.log('duplicate found on right');
-        currentItems[2] = Randomize();
-    }
-    for(var i = 0; i < 3; i++){
-        Product.pics[i].src = Product.all[currentItems[i]].path;
-        Product.pics[i].id = Product.all[currentItems[i]].name;
-        Product.all[currentItems[i]].seen +=1;
-        Product.lastSeen[i] = currentItems[i];
-    }
-};
+  }
+  for(var i = 0; i < 3; i++){
+    Product.pics[i].src = Product.all[currentItems[i]].path;
+    Product.pics[i].id = Product.all[currentItems[i]].name;
+    Product.all[currentItems[i]].seen +=1;
+    Product.lastSeen[i] = currentItems[i];
+  }
+}
 
 function Randomize(){
-    return Math.floor(Math.random() * Product.names.length);
-};
+  return Math.floor(Math.random() * Product.names.length);
+}
 
 //click event handler
 
 function clickHandler(event){
-    if (event.target.id === 'image-container') {
-        return alert('Please click on an item you would like to vote for.');
+  if (event.target.id === 'image-container') {
+    return alert('Please click on an item you would like to vote for.');
+  }
+  Product.clickCount += 1;
+  for (var i = 0; i < Product.names.length; i++){
+    if(event.target.id === Product.all[i].name){
+      Product.all[i].votes += 1;
     }
-    Product.clickCount += 1;
-    for (var i = 0; i < Product.names.length; i++){
-        if(event.target.id === Product.all[i].name){
-            Product.all[i].votes += 1;
-        }
-    }
-    showItems();
-    //strigify for local storage
-    lsProduct = JSON.stringify(Product.all);
-    localStorage.setItem('Products', lsProduct);
-    lsClicks = JSON.stringify(Product.clickCount);
-    localStorage.setItem('Clicks', lsClicks);
+  }
+  showItems();
+  //strigify for local storage
+  lsProduct = JSON.stringify(Product.all);
+  localStorage.setItem('Products', lsProduct);
+  lsClicks = JSON.stringify(Product.clickCount);
+  localStorage.setItem('Clicks', lsClicks);
 
-    // stop and remove after 24 clicks
-    if(Product.clickCount > 24){
-        Product.container.removeEventListener('click', clickHandler);
+  // stop and remove after 24 clicks
+  if(Product.clickCount > 24){
+    Product.container.removeEventListener('click', clickHandler);
 
-        makeChart();
+    makeChart();
 
-        localStorage.removeItem('clickCount');
-    }
-};
+    localStorage.removeItem('clickCount');
+  }
+}
 
 
 
 if (localStorage.Products){
-    getInfo = localStorage.getItem('Products');
-    Product.all = JSON.parse(getInfo);
-    getClick = localStorage.getItem('clickCount');
-    Product.clickCount = JSON.parse(getClick);
-};
+  getInfo = localStorage.getItem('Products');
+  Product.all = JSON.parse(getInfo);
+  getClick = localStorage.getItem('clickCount');
+  Product.clickCount = JSON.parse(getClick);
+}
 
 function makeChart(){
-    var labelColors = ['red', 'blue', 'yellow','green','red', 'blue', 'yellow','green', 'red', 'blue', 'yellow','green', 'red', 'blue', 'yellow','green', 'red', 'blue', 'yellow','green'];
-    var ctx = document.getElementById('chart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar', 
-        data: {
-            labels: Product.names,
-            datasets: [{
-                label: '# of Votes for each Products',
-                data: Product.voteCount,
-                backgroundColor: labelColors
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    })
+  var labelColors = ['red', 'blue', 'yellow','green','red', 'blue', 'yellow','green', 'red', 'blue', 'yellow','green', 'red', 'blue', 'yellow','green', 'red', 'blue', 'yellow','green'];
+  var ctx = document.getElementById('chart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: Product.names,
+      datasets: [{
+        label: '# of Votes for each Products',
+        data: Product.voteCount,
+        backgroundColor: labelColors
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
 
 // clear local storage
 var clearStorage = document.getElementById('clearStorage');
 
 clearStorage.addEventListener('click', function(){
-    console.log('Local Storage Cleared');
-    localStorage.clear();
+  console.log('Local Storage Cleared');
+  localStorage.clear();
 });
 
 buildProducts();
